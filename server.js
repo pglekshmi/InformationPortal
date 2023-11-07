e7228c1d8e1b24150d56daa321e03ed51fe765e8
Aneesh R S <aneesh.rs@duk.ac.in>
2023-11-07 16:42:47 +0530

    Cors Issue Fix

--- server.js
+++ server.js
@@ -3,14 +3,11 @@
 
 // const PORT = 3000;
 
-
-
 // const server = http.createServer((req, res,next) => {
-    
+
 //     res.setHeader('Access-Control-Allow-Origin', '*');
 //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 //     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-   
 
 //     if (req.method === 'POST' && req.url === '/') {
 //         let data = '';
@@ -33,39 +30,62 @@
 //             }
 //         });
 //         // next()
-//     } 
+//     }
 //     else {
 //         res.writeHead(404, { 'Content-Type': 'text/plain' });
 //         res.end('Not Found');
 //     }})
 
-    
-
 //     server.listen(PORT, () => {
 //         console.log(`Server is running on port ${PORT}`);
 //       });
 
-
-      const http = require('http');
-const port = 3000;
-http.createServer((req, res) => {
-    console.log('New Server')
+const http = require("http");
+const PORT = 3000;
+const server = http.createServer((req, res) => {
   const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-    'Access-Control-Max-Age': 2592000, // 30 days
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    "Access-Control-Max-Age": 2592000, // 30 days
     /** add other headers as per requirement */
   };
-  if (req.method === 'OPTIONS') {
-    res.writeHead(204, headers);
-    res.end();
-    return;
+  console.log("req.method", req.method);
+  // if (req.method === "OPTIONS") {
+  //   res.writeHead(204, headers);
+  //   res.end();
+  //   return;
+  // }
+
+  if (req.method === "POST" && req.url === "/") {
+    let data = "";
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+    req.on("end", () => {
+      try {
+        let jsonData = JSON.parse(data);
+        console.log("jsonData", jsonData);
+        res.writeHead(200, headers);
+        res.end(
+          JSON.stringify({
+            data: jsonData,
+            message: "Data received successfully",
+          })
+        );
+        return;
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+        res.writeHead(400, headers);
+        res.end(JSON.stringify({ error: "Invalid JSON data" }));
+      }
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
   }
-  if (['GET', 'POST'].indexOf(req.method) > -1) {
-    res.writeHead(200, headers);
-    res.end('Hello World');
-    return;
-  }
-  res.writeHead(405, headers);
-  res.end(`${req.method} is not allowed for the request.`);
-}).listen(port);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
